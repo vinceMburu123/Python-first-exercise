8976cf4ef2c9d347e3d6d8bea9f2c4e5122bd5a4
vinceMburu123 <judevinny@gmail.com>
2024-05-17 10:03:04 +0300

    first python exercise

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Code for sum of two numbers
+"""
+
+x=int(input("Enter the first Number: "))
+y=int(input("Enter the second Number: "))
+z=x+y
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
