7da983955eee095686fe2b51bfabaef2e113edb0
vinceMburu123 <judevinny@gmail.com>
2024-05-17 10:12:58 +0300

    gross pay program

---
+++ gross.py
@@ -0,0 +1,14 @@
+
+"""
+Code to calculate gross pay
+"""
+hours =35
+rate_per_hour=2.75
+
+gross_pay= hours * rate_per_hour
+
+print(gross_pay)
+
+print(type(hours))
+print(type(rate_per_hour))
+print(type(gross_pay))
