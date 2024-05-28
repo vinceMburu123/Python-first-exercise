b0d318799c04233a87ba79f6e5bf37ce655b42a8
vinceMburu123 <128956764+vinceMburu123@users.noreply.github.com>
2024-05-28 19:42:25 +0300

    Update gross.py

--- gross.py
+++ gross.py
@@ -1,4 +1,4 @@
-#Calculate Gross pay
+#Calculate Gross pay /Just for insert values
 """
 Code to calculate gross pay
 Author:Vincent
@@ -14,3 +14,19 @@ print(gross_pay)
 print(type(hours))
 print(type(rate_per_hour))
 print(type(gross_pay))
+
+#With Prompting inputs
+#Calculate gross pay
+"""
+Code to calculate gross pay
+
+"""
+hours = int(input("Enter the number of hours worked:")) 
+rate_per_hour=int(input("Enter the rate_per_hour :")) 
+
+gross_pay= hours * rate_per_hour
+
+print("Gross Pay:", gross_pay)
+print("Type of hours:", hours)
+print("Type of rate_per_hour:", rate_per_hour)
+print("Type of gross_pay:", gross_pay)
