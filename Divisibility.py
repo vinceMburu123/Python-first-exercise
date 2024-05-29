b2b397fb8d2535033d35c94da8b854a90fbf2ad6
vinceMburu123 <128956764+vinceMburu123@users.noreply.github.com>
2024-05-29 14:54:02 +0300

    Update Divisibility.py

--- Divisibility.py
+++ Divisibility.py
@@ -1,1 +1,32 @@
 
+""
+# Code for Divisibility
+""
+#Code for divisibility 5,7,11
+""
+
+number = int(input("Enter a number: "))
+
+# to track if the number is divisible by any of the divisors
+divisible = False
+
+# Check divisibility
+if number % 5 == 0:
+    print(f"{number} is divisible by 5")
+    divisible = True
+elif number % 7 == 0:
+    print(f"{number} is divisible by 7")
+    divisible = True
+elif number % 11 == 0:
+    print(f"{number} is divisible by 11")
+    divisible = True
+
+# Check if the number is even
+if number % 2 == 0:
+    print(f"{number} is even")
+    divisible = True
+
+# If the number is not divisible by 5, 7, 11, and is not even
+if not divisible:
+    print(f"{number} is not divisible by 5, 7, 11, and is not even")
+
