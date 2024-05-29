4e75ef2eccbc9d9ca5d285378cbb809bfd6c8447
vinceMburu123 <128956764+vinceMburu123@users.noreply.github.com>
2024-05-29 14:52:48 +0300

    Update loan.py

--- loan.py
+++ loan.py
@@ -1,1 +1,15 @@
+ #Code of Loan Eligibility
+""
+#Author:Vincent
+
+""
+
+ #Code of Loan Eligibility
+
+age=int(input("Enter your age:"))
+income=int(input("Enter Income Amount:"))
+if age>=21 and income >=21000:
+    print("Congratulations you qualify for loan:")
+else:
+    print("Unfortunately,we are unable to offer you a loan")
 
